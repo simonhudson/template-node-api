decf5ea9b8ebf6173daf9a7d7118b6120ebd0d79
Simon Hudson <hellosimonhudson@gmail.com>
2024-02-07 15:48:41 +0000

    tidy up

--- api/methods/patch.ts
+++ api/methods/patch.ts
@@ -31,34 +31,3 @@ export const patch = async ({ req, collectionName, db }: PatchParams) => {
 		return createError({ error });
 	}
 };
-
-// import { ApiRequestParams } from '@/api/api';
-// import { createError } from '@/api/utils/createError';
-// import { Db } from 'mongodb';
-// import { OptionalId } from 'mongodb';
-// import { sanitizePayload } from '@/api/utils/sanitizePayload';
-// import { sortObjectByKey } from '@/api/utils/sortObjectByKey';
-
-// interface PostParams extends ApiRequestParams {
-// 	db: Db;
-// }
-
-// export const post = async ({ req, collectionName, db }: PostParams) => {
-// 	const requestBody = req.body;
-
-// 	if (!Object.keys(requestBody).length) return createError('No request body provided');
-
-// 	requestBody.created_at = new Date();
-
-// 	let finalPayload = sortObjectByKey(requestBody);
-// 	finalPayload = sanitizePayload(finalPayload);
-
-// 	try {
-// 		const response = await db
-// 			.collection(collectionName)
-// 			.insertOne(sanitizePayload(finalPayload) as OptionalId<Document>);
-// 		return response;
-// 	} catch (error: unknown) {
-// 		return createError({ error });
-// 	}
-// };
