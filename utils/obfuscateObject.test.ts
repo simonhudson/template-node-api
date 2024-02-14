85c6baec993b7378c84a790663682299ab041a13
Simon Hudson <hellosimonhudson@gmail.com>
2024-02-14 18:11:52 +0000

    obfuscate function

--- utils/obfuscateObject.test.ts
+++ utils/obfuscateObject.test.ts
@@ -1,39 +1,58 @@
 import { obfuscateObject } from './obfuscateObject';
+import type { BaseObject } from '@/types/base';
 
 describe('obfuscateObject', () => {
-	const testObject = {
-		foo: 'foo-value',
-		bar: true,
-		lorem: {
-			ipsum: 'ipsum-value',
-			dolor: 42,
-		},
-		baz: [{ foo: 'foo-value-2' }, { bar: 'bar-value', woof: { foo: 'another-value' } }],
-	};
+	let testObject: BaseObject = {};
 
-	it('should obfuscate string values', () => {
-		console.log(obfuscateObject(testObject, ['foo', 'ipsum']));
+	beforeEach(() => {
+		testObject = {
+			foo: 'foo-value',
+			bar: 'true',
+			lorem: {
+				ipsum: 'ipsum-value',
+				dolor: 42,
+				baz: 'baz-value',
+			},
+			baz: [{ foo: 'foo-value-2' }, { bar: 'bar-value', woof: { foo: 'another-value' } }],
+		};
 	});
 
-	// it('should ob string values', () => {
-	// 	// When
-	// 	const actual = sanitizeObject({
-	// 		foo: '<alert>hello</alert>',
-	// 		bar: true,
-	// 		lorem: {
-	// 			ipsum: ['<alert>hello</alert>', '<alert>goodbye</alert>'],
-	// 			dolor: 42,
-	// 		},
-	// 		ipsum: [{ foo: '<alert>hello</alert>' }, { bar: '<alert>goodbye</alert>' }],
-	// 	});
-	// 	expect(actual).toEqual({
-	// 		foo: '&lt;alert&gt;hello&lt;/alert&gt;',
-	// 		bar: true,
-	// 		lorem: {
-	// 			ipsum: ['&lt;alert&gt;hello&lt;/alert&gt;', '&lt;alert&gt;goodbye&lt;/alert&gt;'],
-	// 			dolor: 42,
-	// 		},
-	// 		ipsum: [{ foo: '&lt;alert&gt;hello&lt;/alert&gt;' }, { bar: '&lt;alert&gt;goodbye&lt;/alert&gt;' }],
-	// 	});
-	// });
+	it('should obfuscate string values based on an allow list', () => {
+		expect(obfuscateObject(testObject, ['foo', 'ipsum'])).toEqual({
+			foo: 'foo-value',
+			bar: '[OBFUSCATED]',
+			lorem: {
+				ipsum: 'ipsum-value',
+				dolor: 42,
+				baz: '[OBFUSCATED]',
+			},
+			baz: [{ foo: 'foo-value-2' }, { bar: '[OBFUSCATED]', woof: { foo: 'another-value' } }],
+		});
+	});
+
+	it('should obfuscate all string values when allow list not provided', () => {
+		expect(obfuscateObject(testObject)).toEqual({
+			foo: '[OBFUSCATED]',
+			bar: '[OBFUSCATED]',
+			lorem: {
+				ipsum: '[OBFUSCATED]',
+				dolor: 42,
+				baz: '[OBFUSCATED]',
+			},
+			baz: [{ foo: '[OBFUSCATED]' }, { bar: '[OBFUSCATED]', woof: { foo: '[OBFUSCATED]' } }],
+		});
+	});
+
+	it('should obfuscate all string values with provided replacement', () => {
+		expect(obfuscateObject(testObject, undefined, 'some-replacement')).toEqual({
+			foo: 'some-replacement',
+			bar: 'some-replacement',
+			lorem: {
+				ipsum: 'some-replacement',
+				dolor: 42,
+				baz: 'some-replacement',
+			},
+			baz: [{ foo: 'some-replacement' }, { bar: 'some-replacement', woof: { foo: 'some-replacement' } }],
+		});
+	});
 });
--- utils/obfuscateObject.ts
+++ utils/obfuscateObject.ts
@@ -3,14 +3,11 @@ import type { BaseObject } from '@/types/base';
 
 export const obfuscateObject = (
 	obj: BaseObject,
-	allowList: string[],
+	allowList?: string[],
 	replacementValue: string = '[OBFUSCATED]'
 ): BaseObject => {
 	traverse(obj).forEach(function (this: any, item: any) {
-		allowList.forEach((key) => {
-			if (this.key !== key) this.update(replacementValue);
-		});
+		if (typeof item === 'string' && !allowList?.includes(this.key)) this.update(replacementValue);
 	});
-
 	return obj;
 };
