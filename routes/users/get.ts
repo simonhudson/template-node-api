24fe8844aa1930fb7aa6a1451e814da98214df66
Simon Hudson <hellosimonhudson@gmail.com>
2024-02-20 13:32:03 +0000

    transform func

--- routes/users/get.ts
+++ routes/users/get.ts
@@ -2,6 +2,15 @@ import { COLLECTION_NAME } from './constants';
 import { makeRequest } from '@/utils/makeRequest';
 import type { Request, Response } from 'express';
 
+const transform = async (data: any) => {
+	console.log('x----------------');
+	console.log(data);
+	console.log('/x----------------');
+	return data;
+	// // Add slug and age to the request body
+	// requestBody.age = dayjs().diff(dayjs(requestBody.date_of_birth), 'year');
+};
+
 export const get = async (req: Request, res: Response): Promise<void> => {
 	const userSlug = req.params.slug;
 	const query = userSlug ? { slug: userSlug } : {};
