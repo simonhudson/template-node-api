77cd71eabf4d41e5ddcadef895331a635a177610
Simon Hudson <hellosimonhudson@gmail.com>
2024-02-22 11:58:12 +0000

    remove dist

--- dist/routes/users/patch.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.patch = void 0;
-const constants_1 = require("./constants");
-const makeRequest_1 = require("@/utils/makeRequest");
-const patch = async (req, res) => {
-    res.json(await (0, makeRequest_1.makeRequest)({ req, res, collectionName: constants_1.COLLECTION_NAME }));
-};
-exports.patch = patch;
