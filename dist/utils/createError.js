d0765370f5679da347c5a92b25e0338f01ed5173
Simon Hudson <hellosimonhudson@gmail.com>
2024-02-23 11:47:34 +0000

    remove dist

--- dist/utils/createError.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.createError = void 0;
-const createError = ({ message, data }) => {
-    return { error: { message, data } };
-};
-exports.createError = createError;
