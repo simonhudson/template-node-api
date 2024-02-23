b0d7923eea365990c48aae2b43ee4ebe4da4aebc
Simon Hudson <hellosimonhudson@gmail.com>
2024-02-23 10:46:45 +0000

    remove dist

--- dist/utils/sanitizeString.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.sanitizeString = void 0;
-const xss_1 = __importDefault(require("xss"));
-const sanitizeString = (value) => (value ? (0, xss_1.default)(value) : undefined);
-exports.sanitizeString = sanitizeString;
