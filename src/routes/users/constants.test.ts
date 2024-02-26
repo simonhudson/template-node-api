e8d10fc3b6d5281f028fcc0a89bc979305a63f19
Simon Hudson <hellosimonhudson@gmail.com>
2024-02-26 07:46:55 +0000

    fix tests

--- src/routes/users/constants.test.ts
+++ src/routes/users/constants.test.ts
@@ -3,7 +3,7 @@ import * as constants from './constants';
 describe('constants', () => {
 	it('should return expected values', () => {
 		expect(constants).toEqual({
-			COLLECTION_NAME: 'usersx',
+			COLLECTION_NAME: 'users',
 		});
 	});
 });
