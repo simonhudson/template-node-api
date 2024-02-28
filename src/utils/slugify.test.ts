60142b8a694eb2a33b1434985eafd522aa20a8a2
Simon Hudson <hellosimonhudson@gmail.com>
2024-02-28 15:30:59 +0000

    update slugify

--- src/utils/slugify.test.ts
+++ src/utils/slugify.test.ts
@@ -4,7 +4,8 @@ describe('slugify', () => {
 	[
 		{ input: 'Foo', expected: 'foo' },
 		{ input: 'Foo Bar', expected: 'foo-bar' },
-		{ input: `  123Foo456Bar_hello Lorem ipsum--dolor  x  `, expected: '123foo456bar_hello-lorem-ipsum--dolor-x' },
+		{ input: `  123Foo456Bar_hello Lorem ipsum--dolor  x  `, expected: '123foo456bar-hello-lorem-ipsum-dolor-x' },
+		{ input: `Design & Engineering`, expected: 'design-engineering' },
 	].forEach((scenario) => {
 		it(`should return ${scenario.expected} for input ${scenario.input}`, () => {
 			expect(slugify(scenario.input)).toEqual(scenario.expected);
--- src/utils/slugify.ts
+++ src/utils/slugify.ts
@@ -2,4 +2,5 @@ export const slugify = (str: string): string =>
 	str
 		.toLowerCase()
 		.trim()
-		.replace(/[ ]{1,}/g, '-');
+		.replace(/[^a-zA-Z0-9]/g, '-')
+		.replace(/-{2,}/g, '-');
