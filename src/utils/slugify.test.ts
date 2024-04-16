5529a28179183b0aaf88c0e4976a833d602f7916
Simon Hudson <hellosimonhudson@gmail.com>
2024-04-16 09:13:46 +0100

    strip apos from slug

--- src/utils/slugify.test.ts
+++ src/utils/slugify.test.ts
@@ -6,6 +6,8 @@ describe('slugify', () => {
 		{ input: 'Foo Bar', expected: 'foo-bar' },
 		{ input: `  123Foo456Bar_hello Lorem ipsum--dolor  x  `, expected: '123foo456bar-hello-lorem-ipsum-dolor-x' },
 		{ input: `Design & Engineering`, expected: 'design-engineering' },
+		{ input: `Samuel E'to`, expected: 'samuel-eto' },
+		{ input: `S'amuel E'to`, expected: 'samuel-eto' },
 	].forEach((scenario) => {
 		it(`should return ${scenario.expected} for input ${scenario.input}`, () => {
 			expect(slugify(scenario.input)).toEqual(scenario.expected);
